92ca310384e59d8314858fc4ba10dad9a0336acf
Ivan Gavrilov <ivan-gavrilov-m@yandex.ru>
2023-03-02 20:56:57 +0400

    feat: #6 use PathRouter

--- src/core/index.ts
+++ src/core/index.ts
@@ -2,5 +2,6 @@ export { default as Block, BlockClass } from './Block';
 export { default as registerComponent } from './registerComponent';
 export { default as renderDOM } from './renderDOM';
 export { HashRouter } from './Router/HashRouter';
-export { CoreRouter } from './Router/CoreRouter';
+export { PathRouter } from './Router/PathRouter';
+export type { CoreRouter } from './Router/CoreRouter';
 export { Store, Dispatch } from './Store';
