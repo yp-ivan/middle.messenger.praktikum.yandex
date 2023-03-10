9e7287c53dd610ef002925ba0f12ebf68cf684bf
Ivan Gavrilov <ivan-gavrilov-m@yandex.ru>
2023-03-10 12:01:39 +0400

    refactor: #6 add comments

--- src/helpers/utils/isEqual.ts
+++ src/helpers/utils/isEqual.ts
@@ -17,6 +17,7 @@ const isArrayOrObject = (value: unknown): value is [] | PlainObject => {
   return isPlainObject(value) || isArray(value);
 };
 
+// Сравнение объектов по содержимому
 const isEqual = (lhs: PlainObject, rhs: PlainObject): boolean => {
   if (Object.keys(lhs).length !== Object.keys(rhs).length) {
     return false;
@@ -24,6 +25,7 @@ const isEqual = (lhs: PlainObject, rhs: PlainObject): boolean => {
 
   for (const [key, value] of Object.entries(lhs)) {
     const rightValue = rhs[key];
+    // Сначала проверяем, не объекты ли
     if (isArrayOrObject(value) && isArrayOrObject(rightValue)) {
       if (isEqual(value, rightValue)) {
         continue;
