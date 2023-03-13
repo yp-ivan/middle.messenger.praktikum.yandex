f8c11d76f95f8f334fef21848b9ac547bb5e4c2e
Ivan Gavrilov <ivan-gavrilov-m@yandex.ru>
2023-03-13 10:57:24 +0400

    feat: #9 Webpack install

---
+++ webpack.config.js
@@ -0,0 +1,72 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+const stylesHandler = MiniCssExtractPlugin.loader;
+
+const config = {
+  entry: './src/index.ts',
+  output: {
+    path: path.resolve(__dirname, 'dist')
+  },
+  devServer: {
+    open: true,
+    host: 'localhost',
+    historyApiFallback: true
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      favicon: './src/assets/favicon.svg'
+    }),
+    new MiniCssExtractPlugin()
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/i,
+        loader: 'ts-loader',
+        exclude: ['/node_modules/']
+      },
+      {
+        test: /\.css$/i,
+        use: [stylesHandler, 'css-loader']
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [stylesHandler, 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+        type: 'asset'
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+    alias: {
+      handlebars: 'handlebars/dist/handlebars.min.js',
+      assets: path.resolve(__dirname, 'src/assets'),
+      api: path.resolve(__dirname, 'src/api'),
+      components: path.resolve(__dirname, 'src/components'),
+      core: path.resolve(__dirname, 'src/core'),
+      data: path.resolve(__dirname, 'src/data'),
+      helpers: path.resolve(__dirname, 'src/helpers'),
+      pages: path.resolve(__dirname, 'src/pages'),
+      router: path.resolve(__dirname, 'src/router'),
+      services: path.resolve(__dirname, 'src/services'),
+      store: path.resolve(__dirname, 'src/store')
+    }
+  }
+};
+
+module.exports = () => {
+  if (isProduction) {
+    config.mode = 'production';
+  } else {
+    config.mode = 'development';
+  }
+  return config;
+};
