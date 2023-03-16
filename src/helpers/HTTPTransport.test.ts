4ffa36d8ea52c72cd54beaa962f04899b1837339
Ivan Gavrilov <ivan-gavrilov-m@yandex.ru>
2023-03-16 14:42:37 +0400

    feat: #9 update tests to HTTPTransport (use mockRequests)

--- src/helpers/HTTPTransport.test.ts
+++ src/helpers/HTTPTransport.test.ts
@@ -1,26 +1,31 @@
+import { server } from 'tests/apiMock';
 import HTTPTransport from './HTTPTransport';
 
-const endpoint = 'https://jsonplaceholder.typicode.com/posts';
-const data = { userId: 10, title: 'Test' };
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
+
+const endpoint = `${process.env.API_ENDPOINT}/mock`;
+const data = { check: 'Test' };
 
 describe('helpers/HTTPTransport', () => {
   it('GET request', async () => {
-    const { responseText } = await HTTPTransport.get(`${endpoint}/1`);
-    expect(JSON.parse(responseText).id).toBe(1);
+    const { responseText } = await HTTPTransport.get(`${endpoint}/get`);
+    expect(JSON.parse(responseText).type).toBe('GET');
   });
 
   it('POST request', async () => {
-    const { responseText } = await HTTPTransport.post(`${endpoint}`, { data });
-    expect(JSON.parse(responseText).id).toBe(101);
+    const { responseText } = await HTTPTransport.post(`${endpoint}/post`, { data });
+    expect(JSON.parse(responseText).type).toBe('POST');
   });
 
   it('PUT request', async () => {
-    const { responseText } = await HTTPTransport.put(`${endpoint}/1`, { data });
-    expect(JSON.parse(responseText).id).toBe(1);
+    const { responseText } = await HTTPTransport.put(`${endpoint}/put`, { data });
+    expect(JSON.parse(responseText).type).toBe('PUT');
   });
 
   it('DELETE request', async () => {
-    const { responseText } = await HTTPTransport.delete(`${endpoint}/1`, { data });
-    expect(responseText).toBe('{}');
+    const { responseText } = await HTTPTransport.delete(`${endpoint}/delete`, { data });
+    expect(JSON.parse(responseText).type).toBe('DELETE');
   });
 });
